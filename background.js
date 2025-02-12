72f6974fed4e8ef891cb19548002cfc2437f1228
Kamal Thakur <kamal.thakur.dev16@gmail.com>
2025-02-12 13:09:02 +0530

    -Host Permission Fix -Lightning Setup Domain support Added

--- background.js
+++ background.js
@@ -20,18 +20,17 @@ chrome.action.onClicked.addListener((activeTab) => {
 
   const developerConsoleUrl = baseURL + "/_ui/common/apex/debug/ApexCSIPage";
   const anonymousUrl = baseURL + "/_ui/common/apex/debug/ApexExecAnon";
-  chrome.tabs.query({ url: developerConsoleUrl }, (tabs) => {
-    if (tabs && tabs.length > 0) {
-      const devConsoleTabId = tabs[0].id;
-      chrome.scripting.executeScript({
-        target: { tabId: devConsoleTabId },
-        func: (anonUrl) => {
-          window.open(anonUrl, '_blank');
-        },
-        args: [anonymousUrl]
-      });
-    } else {
-      chrome.tabs.create({ url: developerConsoleUrl });
-    }
-  });
+
+  // Instead of querying all tabs, check if the active tab is the developer console.
+  if (activeURL === developerConsoleUrl) {
+    chrome.scripting.executeScript({
+      target: { tabId: activeTab.id },
+      func: (anonUrl) => {
+        window.open(anonUrl, '_blank');
+      },
+      args: [anonymousUrl]
+    });
+  } else {
+    chrome.tabs.create({ url: developerConsoleUrl });
+  }
 });
